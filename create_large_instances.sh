5ffa877893f2eeec15b0619048c33f6596c6b93a
Oliver Hinder <ohinder@gmail.com>
2024-01-12 15:29:31 -0500

    Minor fixes to scripts + gitignore

--- create_large_instances.sh
+++ create_large_instances.sh
@@ -1,30 +1,33 @@
 mkdir large-problem-instances
 
-julia-1.7 generate-multicommodity-flow.jl \
+julia generate-multicommodity-flow.jl \
     --output_file large-problem-instances/multicommodity-flow-small-test-instance.mps.gz \
     --num_commodities 40000 \
     --num_warehouses 100 \
     --num_stores 2000
 
-julia-1.7 generate-heat-source-location.jl \
+julia generate-heat-source-location.jl \
     --output_file large-problem-instances/heat-source-instance1.mps.gz \
-    --ground_truth_file large-problem-instances/temperature_ground_truth.txt \
+    --ground_truth_file large-problem-instances/temperature_ground_truth1.txt \
     --grid_size 500 \
     --num_source_locations 10 \
     --num_possible_source_locations 500 \
+     --seed 1 \
     --num_measurement_locations 100
 
-julia-1.7 generate-heat-source-location.jl \
+julia generate-heat-source-location.jl \
     --output_file large-problem-instances/heat-source-instance2.mps.gz \
-    --ground_truth_file large-problem-instances/temperature_ground_truth.txt \
+    --ground_truth_file large-problem-instances/temperature_ground_truth2.txt \
     --grid_size 500 \
     --num_source_locations 10 \
     --num_possible_source_locations 500 \
+    --seed 2 \
     --num_measurement_locations 50
 
-julia-1.7 design-matching-synthetic.jl \
-    --output_file test_design_match.mps.gz \
-    --epsilon 0.00141 \ : '1/sqrt(500000)'
+# Note: we calculate epsilon = 1/sqrt(num_treatment_samples) = 0.00141
+julia design-matching-synthetic.jl \
+    --output_file large-problem-instances/synthetic-design-match.mps.gz \
+    --epsilon 0.00141 \
     --num_treatment_samples 500000 \
     --num_control_samples 5000000 \
     --num_covariates 15 \
--- create_small_instances.sh
+++ create_small_instances.sh
@@ -1,30 +1,34 @@
 mkdir small-problem-instances
 
-julia-1.7 generate-multicommodity-flow.jl \
+julia generate-multicommodity-flow.jl \
     --output_file small-problem-instances/multicommodity-flow-small-test-instance.mps.gz \
     --num_commodities 100 \
     --num_warehouses 30 \
     --num_stores 100
 
-julia-1.7 generate-heat-source-location.jl \
+julia generate-heat-source-location.jl \
     --output_file small-problem-instances/heat-source-instance1.mps.gz \
-    --ground_truth_file small-problem-instances/temperature_ground_truth.txt \
+    --ground_truth_file small-problem-instances/temperature_ground_truth1.txt \
     --grid_size 50 \
     --num_source_locations 3 \
     --num_possible_source_locations 100 \
+    --seed 1 \
     --num_measurement_locations 80
 
-julia-1.7 generate-heat-source-location.jl \
+julia generate-heat-source-location.jl \
     --output_file small-problem-instances/heat-source-instance2.mps.gz \
-    --ground_truth_file small-problem-instances/temperature_ground_truth.txt \
+    --ground_truth_file small-problem-instances/temperature_ground_truth2.txt \
     --grid_size 50 \
     --num_source_locations 3 \
     --num_possible_source_locations 100 \
+    --seed 2 \
     --num_measurement_locations 40
+    
 
-julia-1.7 design-matching-synthetic.jl \
-    --output_file test_design_match.mps.gz \
-    --epsilon 0.0141 \ : '1/sqrt(5000)'
+# Note: we calculate epsilon = 1/sqrt(num_treatment_samples) = 0.0141
+julia design-matching-synthetic.jl \
+    --output_file small-problem-instances/synthetic-design-match.mps.gz \
+    --epsilon 0.0141 \
     --num_treatment_samples 5000 \
     --num_control_samples 20000 \
     --num_covariates 8 \
