02971612861112d1909f0ae0d58d6742a9259004
Mateo Diaz <mateodd@icloud.com>
2024-01-15 10:33:15 -0500

    Fix SCS param eps, and script for large instances

--- create_small_instances.sh
+++ create_small_instances.sh
@@ -1,36 +1,35 @@
 mkdir small-problem-instances
 
-julia generate-multicommodity-flow.jl \
-    --output_file small-problem-instances/multicommodity-flow-small-test-instance.mps.gz \
-    --num_commodities 100 \
-    --num_warehouses 30 \
-    --num_stores 100
+julia --project generate-multicommodity-flow.jl \
+  --output_file small-problem-instances/multicommodity-flow-small-test-instance.mps.gz \
+  --num_commodities 100 \
+  --num_warehouses 30 \
+  --num_stores 100
 
-julia generate-heat-source-location.jl \
-    --output_file small-problem-instances/heat-source-instance1.mps.gz \
-    --ground_truth_file small-problem-instances/temperature_ground_truth1.txt \
-    --grid_size 50 \
-    --num_source_locations 3 \
-    --num_possible_source_locations 100 \
-    --seed 1 \
-    --num_measurement_locations 80
+julia --project generate-heat-source-location.jl \
+  --output_file small-problem-instances/heat-source-instance1.mps.gz \
+  --ground_truth_file small-problem-instances/temperature_ground_truth1.txt \
+  --grid_size 50 \
+  --num_source_locations 3 \
+  --num_possible_source_locations 100 \
+  --seed 1 \
+  --num_measurement_locations 80
 
-julia generate-heat-source-location.jl \
-    --output_file small-problem-instances/heat-source-instance2.mps.gz \
-    --ground_truth_file small-problem-instances/temperature_ground_truth2.txt \
-    --grid_size 50 \
-    --num_source_locations 3 \
-    --num_possible_source_locations 100 \
-    --seed 2 \
-    --num_measurement_locations 40
-    
+julia --project generate-heat-source-location.jl \
+  --output_file small-problem-instances/heat-source-instance2.mps.gz \
+  --ground_truth_file small-problem-instances/temperature_ground_truth2.txt \
+  --grid_size 50 \
+  --num_source_locations 3 \
+  --num_possible_source_locations 100 \
+  --seed 2 \
+  --num_measurement_locations 40
 
 # Note: we calculate epsilon = 1/sqrt(num_treatment_samples) = 0.0141
-julia design-matching-synthetic.jl \
-    --output_file small-problem-instances/synthetic-design-match.mps.gz \
-    --epsilon 0.0141 \
-    --num_treatment_samples 5000 \
-    --num_control_samples 20000 \
-    --num_covariates 8 \
-    --num_edges_per_treatment 10 \
-    --control_shift_magnitude 0.1
+julia --porject design-matching-synthetic.jl \
+  --output_file small-problem-instances/synthetic-design-match.mps.gz \
+  --epsilon 0.0141 \
+  --num_treatment_samples 5000 \
+  --num_control_samples 20000 \
+  --num_covariates 8 \
+  --num_edges_per_treatment 10 \
+  --control_shift_magnitude 0.1
--- generate-heat-source-location.jl
+++ generate-heat-source-location.jl
@@ -9,41 +9,41 @@ using JuMP, SCS
 
 function build_discretized_possion!(model, u, q, N::Int64)
     h = 1.0 / N
-    @expression(model, grad_u_xx[i=1:N,j=1:N,k=1:N], (u[i+1,j,k] - 2 * u[i,j,k] + u[i-1,j,k]) / h^2);
-    @expression(model, grad_u_yy[i=1:N,j=1:N,k=1:N], (u[i,j+1,k] - 2 * u[i,j,k] + u[i,j-1,k]) / h^2);
-    @expression(model, grad_u_zz[i=1:N,j=1:N,k=1:N], (u[i,j,k+1] - 2 * u[i,j,k] + u[i,j,k-1]) / h^2);
-    @constraint(model, grad_u_xx + grad_u_yy + grad_u_zz .== -q);
+    @expression(model, grad_u_xx[i=1:N, j=1:N, k=1:N], (u[i+1, j, k] - 2 * u[i, j, k] + u[i-1, j, k]) / h^2)
+    @expression(model, grad_u_yy[i=1:N, j=1:N, k=1:N], (u[i, j+1, k] - 2 * u[i, j, k] + u[i, j-1, k]) / h^2)
+    @expression(model, grad_u_zz[i=1:N, j=1:N, k=1:N], (u[i, j, k+1] - 2 * u[i, j, k] + u[i, j, k-1]) / h^2)
+    @constraint(model, grad_u_xx + grad_u_yy + grad_u_zz .== -q)
 
     # boundary conditions
     for j = 0:(N+1)
         for k = 0:(N+1)
-            JuMP.fix(u[0,j,k], 0.0; force=true)
-            JuMP.fix(u[N+1,j,k], 0.0; force=true)
+            JuMP.fix(u[0, j, k], 0.0; force=true)
+            JuMP.fix(u[N+1, j, k], 0.0; force=true)
         end
     end
 
     for i = 0:(N+1)
         for k = 0:(N+1)
-            JuMP.fix(u[i,0,k], 0.0; force=true)
-            JuMP.fix(u[i,N+1,k], 0.0; force=true)
+            JuMP.fix(u[i, 0, k], 0.0; force=true)
+            JuMP.fix(u[i, N+1, k], 0.0; force=true)
         end
     end
 
     for i = 0:(N+1)
         for j = 0:(N+1)
-            JuMP.fix(u[i,j,0], 0.0; force=true)
-            JuMP.fix(u[i,j,N+1], 0.0; force=true)
+            JuMP.fix(u[i, j, 0], 0.0; force=true)
+            JuMP.fix(u[i, j, N+1], 0.0; force=true)
         end
     end
 end
 
 function build_heat_source_detection_problem(
-        num_source_locations::Int64,
-        num_possible_source_locations::Int64,
-        num_measurement_locations::Int64,
-        grid_size::Int64,
-        maximum_relative_measurement_error::Float64
-    )
+    num_source_locations::Int64,
+    num_possible_source_locations::Int64,
+    num_measurement_locations::Int64,
+    grid_size::Int64,
+    maximum_relative_measurement_error::Float64
+)
     @assert num_source_locations < num_possible_source_locations
     @assert grid_size > 2
     @assert num_measurement_locations > num_source_locations
@@ -54,24 +54,25 @@ function build_heat_source_detection_problem(
     heat_flow_rate = rand(num_source_locations)
 
     q = zeros(grid_size, grid_size, grid_size)
-    for location=axes(heat_source_location_indexes, 2)
-        q[heat_source_location_indexes[:,location]...] += grid_size^3 * heat_flow_rate[location]
+    for location = axes(heat_source_location_indexes, 2)
+        q[heat_source_location_indexes[:, location]...] += grid_size^3 * heat_flow_rate[location]
     end
 
     ##################
     # Compute u_true #
     ##################
-    
+
     # TODO(ohinder): replace SCS with pure conjugate gradient???
     pde_model = Model(optimizer_with_attributes(SCS.Optimizer,
-       "max_iters" => 100000,
-       "eps" => 10^-10,
-       "linear_solver" => SCS.IndirectSolver
+        "max_iters" => 100000,
+        "eps_abs" => 10^-10,
+        "eps_rel" => 10^-10,
+        "linear_solver" => SCS.IndirectSolver
     ))
 
     println("computing u_true")
-    
-    @variable(pde_model, u[i=0:(grid_size+1),j=0:(grid_size+1),k=0:(grid_size+1)]);
+
+    @variable(pde_model, u[i=0:(grid_size+1), j=0:(grid_size+1), k=0:(grid_size+1)])
     build_discretized_possion!(pde_model, u, q, grid_size)
     optimize!(pde_model)
     u_true = Array(value.(u))
@@ -81,10 +82,10 @@ function build_heat_source_detection_problem(
     println("building inverse problem ...")
     measurement_locations = rand(3, num_measurement_locations)
     measurement_location_indexes = Int.(round.((grid_size - 1) * measurement_locations)) .+ 1
-    
+
     candidate_locations = rand(3, num_possible_source_locations - num_source_locations)
     candidate_location_indexes = Int.(round.((grid_size - 1) * candidate_locations)) .+ 1
-    
+
 
     ############################
     # Build optimization model #
@@ -92,31 +93,32 @@ function build_heat_source_detection_problem(
     println("building model ...")
 
     model = Model(optimizer_with_attributes(SCS.Optimizer,
-       "max_iters" => 100000,
-       "eps" => 10^-6,
-       "linear_solver" => SCS.IndirectSolver
+        "max_iters" => 100000,
+        "eps_abs" => 10^-6,
+        "eps_rel" => 10^-6,
+        "linear_solver" => SCS.IndirectSolver
     ))
-    @variable(model, u[i=0:(grid_size+1),j=0:(grid_size+1),k=0:(grid_size+1)]);
-    @variable(model, 0.0 <= q[i=1:grid_size,j=1:grid_size,k=1:grid_size] <= 0.0);
+    @variable(model, u[i=0:(grid_size+1), j=0:(grid_size+1), k=0:(grid_size+1)])
+    @variable(model, 0.0 <= q[i=1:grid_size, j=1:grid_size, k=1:grid_size] <= 0.0)
 
-    @objective(model, Min, sum(q));
+    @objective(model, Min, sum(q))
 
     # second-order central difference
     build_discretized_possion!(model, u, q, grid_size)
 
-    # q is unknown at possible heat source locations 
-    for location=axes(heat_source_location_indexes, 2)
-        location_indicies = heat_source_location_indexes[:,location] 
+    # q is unknown at possible heat source locations
+    for location = axes(heat_source_location_indexes, 2)
+        location_indicies = heat_source_location_indexes[:, location]
         JuMP.set_upper_bound(q[location_indicies...], grid_size^3)
     end
-    for location=axes(candidate_location_indexes, 2)
-        location_indicies = candidate_location_indexes[:,location]
+    for location = axes(candidate_location_indexes, 2)
+        location_indicies = candidate_location_indexes[:, location]
         JuMP.set_upper_bound(q[location_indicies...], grid_size^3)
     end
 
-    # u is known at the measurement locations 
-    for location=axes(measurement_location_indexes,2)
-        location_indicies = measurement_location_indexes[:,location]
+    # u is known at the measurement locations
+    for location = axes(measurement_location_indexes, 2)
+        location_indicies = measurement_location_indexes[:, location]
         # Allow a small amount of errors in measurments
         # to ensure that errors in the calculation of u_true do not
         # make the model infeasible.
@@ -136,34 +138,34 @@ function parse_commandline()
 
     @add_arg_table s begin
         "--seed"
-            help = "The random seed used to generate the instance."
-            arg_type = Int64
-            default = 1
+        help = "The random seed used to generate the instance."
+        arg_type = Int64
+        default = 1
         "--num_source_locations"
-            arg_type = Int64
-            default = 2
+        arg_type = Int64
+        default = 2
         "--num_possible_source_locations"
-            arg_type = Int64
-            default = 50
+        arg_type = Int64
+        default = 50
         "--num_measurement_locations"
-            arg_type = Int64
-            default = 20
+        arg_type = Int64
+        default = 20
         "--maximum_relative_measurement_error"
-            arg_type = Float64
-            default = 1e-6
+        arg_type = Float64
+        default = 1e-6
         "--grid_size"
-            help = "Number of grid planes per coordinate. The total number of grid points 
-            will be the square of this number."
-            arg_type = Int64
-            default = 7
+        help = "Number of grid planes per coordinate. The total number of grid points
+        will be the square of this number."
+        arg_type = Int64
+        default = 7
         "--ground_truth_file"
-            help = "This is the file that will include the data for the ground truth value of u.
-            This is a HDF5 file."
-            arg_type = String
-            required = true
+        help = "This is the file that will include the data for the ground truth value of u.
+        This is a HDF5 file."
+        arg_type = String
+        required = true
         "--output_file"
-            help = "This is the location that the mps file will be written to."
-            required = true
+        help = "This is the location that the mps file will be written to."
+        required = true
     end
 
     return parse_args(s)
@@ -172,7 +174,7 @@ end
 function main()
     parsed_args = parse_commandline()
     println("Parsed args:")
-    for (arg,val) in parsed_args
+    for (arg, val) in parsed_args
         println("  $arg  =>  $val")
     end
 
